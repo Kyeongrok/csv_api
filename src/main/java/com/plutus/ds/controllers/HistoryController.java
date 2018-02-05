61589362551c4b032e3047e418197e4b3592b147
kyeongrok <oceanfog1@gmail.com>
2018-02-05 09:52:23 +0900

    controller연결

---
+++ src/main/java/com/plutus/ds/controllers/HistoryController.java
@@ -0,0 +1,32 @@
+package com.plutus.ds.controllers;
+
+
+import com.plutus.ds.service.CandleChartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "*")
+public class HistoryController {
+
+    @Autowired
+    private CandleChartService candleChartService;
+
+    @GetMapping("history")
+    public String getArgs(
+            @RequestParam(value = "resolution")String resolution,
+            @RequestParam(value = "from")String from,
+            @RequestParam(value = "to")String to) {
+
+
+        return resolution + ":" + from + ":" + to;
+    }
+}
---
+++ src/main/java/com/plutus/ds/service/CandleChartService.java
@@ -0,0 +1,8 @@
+package com.plutus.ds.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CandleChartService {
+
+}
